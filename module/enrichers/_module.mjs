6772a8fbd8b12b48dc9be634b64496663f38c1ce
WBHarry <williambjrklund@gmail.com>
2025-07-19 20:15:49 +0200

    Added DamageEnricher

--- module/enrichers/_module.mjs
+++ module/enrichers/_module.mjs
@@ -1,2 +1,20 @@
-export { default as DhDualityRollEnricher } from './DualityRollEnricher.mjs';
-export { default as DhTemplateEnricher } from './TemplateEnricher.mjs';
+import { default as DhDamageEnricher } from './DamageEnricher.mjs';
+import { default as DhDualityRollEnricher } from './DualityRollEnricher.mjs';
+import { default as DhTemplateEnricher } from './TemplateEnricher.mjs';
+
+export { DhDamageEnricher, DhDualityRollEnricher, DhTemplateEnricher };
+
+export const enricherConfig = [
+    {
+        pattern: /^@Damage\[(.*)\]$/g,
+        enricher: DhDamageEnricher
+    },
+    {
+        pattern: /\[\[\/dr\s?(.*?)\]\]/g,
+        enricher: DhDualityRollEnricher
+    },
+    {
+        pattern: /^@Template\[(.*)\]$/g,
+        enricher: DhTemplateEnricher
+    }
+];
